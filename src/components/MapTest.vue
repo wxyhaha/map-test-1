298fc06126ab0facbb559a4ad56643585a90b809
joel <1666464510@qq.com>
2022-05-26 23:42:25 +0800

    地图点击事件

--- src/components/MapTest.vue
+++ src/components/MapTest.vue
@@ -14,12 +14,17 @@ export default {
   },
   mounted() {
     this.map = this.$utils.map.createMap("map-container");
-
     // 加载 open street map 图层服务
     this.$utils.map.createTileLayer(this.map, this.OSMUrl, {});
 
     // 设置地图视图 中心位置
-    this.map.setView([30.524986, 114.358417], 13);
+    this.map.setView([30.524986, 114.358417], 14);
+    //添加标记
+    this.$utils.map.createMarker(this.map);
+    //添加一个圆
+    this.$utils.map.createCircle(this.map);
+    //添加多边形
+    this.$utils.map.createPolygon(this.map);
   }
 };
 </script>
--- src/utils/map.js
+++ src/utils/map.js
@@ -2,12 +2,48 @@ import "leaflet/dist/leaflet.css"
 import $L from "leaflet";
 
 const createMap = (divId, options) => {
-  return $L.map(divId, options);
+  let map=$L.map(divId, options)
+  var popup = $L.popup();
+  function onMapClick(e) {
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+  }
+
+  map.on('click', onMapClick);
+  return map;
 };
 const createTileLayer = async (map, url, options) => {
   let tileLayer = await $L.tileLayer(url, options);
   tileLayer.addTo(map);
   return tileLayer;
 };
+const createCircle=(map)=>{
+  let circle=$L.circle([30.524986, 114.358417], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.1,
+    radius: 500
+  }).addTo(map)
+  circle.bindPopup("这是一个圆形区域");
+  return circle
+}
+const createPolygon=(map)=>{
+  let polygon=$L.polygon([
+    [30.526188, 114.374413],
+    [30.524154, 114.369178],
+    [30.520827, 114.368234],
+    [30.518831, 114.376731],
+    [30.522602, 114.376388]
+  ]).addTo(map);
+  polygon.bindPopup("这是一个多边形区域");
+  return polygon
+}
+const createMarker=(map)=>{
+  let marker =$L.marker([30.527351, 114.367986]).addTo(map);
+  marker.bindPopup("这是一个标记");
+  return marker
+}
 
-export default { createMap,createTileLayer };
+export default { createMap,createTileLayer,createCircle,createPolygon,createMarker};
